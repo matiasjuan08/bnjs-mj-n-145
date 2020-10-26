40063bcdc86398d6f583af497f824242437defb4
Matias <matiasmigueljuan@gmail.com>
2020-10-26 13:30:41 -0300

    bnjs-mj-n-145 version 1

--- Desktop/Desarrollo web/EANT/bnjs-mj-n-145/app.js
+++ Desktop/Desarrollo web/EANT/bnjs-mj-n-145/app.js
@@ -1,14 +1,38 @@
-const http = require ('http');
+
+const express = require ('express')
 const port = 1000
+// const port2 = 2000
+
+const app = express()
+app.listen(port)
+
+app.use (express.urlencoded({ extended : true}))
 
 
+app.use(express.static('public'))
 
-const server = (req, res) => {
+// app.get('/contacto', (req,res) => {
+//     res.end ('Aca va a estar mi contacto')
+// })
+
+app.post('/enviar', (req,res) => {
+    res.end ('ALgo muy loco')
+})
+
+// const server = (req, res) => {
+//     fs.readFile('front/index.html', (error, content) => {
+//         if(error){
+//             res.writeHead(404, {'content-type' : 'text-plain'} )
+//             res.end ('salio mal')
+//         } else {
+//             res.writeHead(200, {'content-type' : 'text-html'})
+//             res.end (content)
+//         }
+//     })
     
-    res.end('Matias');
-}
+// }
 
-http.createServer(server).listen(port)
+// http.createServer(server).listen(port2)
 
 
 
